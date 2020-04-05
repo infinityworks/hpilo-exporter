65aca9d27b0bb29611073d237591a05a0a984af8
Grosjean Cyril <cygrosjean@gmail.com>
2020-04-05 10:27:36 +0200

    Correct tab spacing and add host IP if server_name is empty (#9)
    * Correct tab spacing and add host IP if server_name is empty
    
    * Update exporter.py
    
    Fix whitespace
    
    * Update exporter.py
    
    Further whitespace fix
    
    Co-authored-by: Joe Stringer <joeds13@users.noreply.github.com>

--- src/hpilo_exporter/exporter.py
+++ src/hpilo_exporter/exporter.py
@@ -88,16 +88,16 @@ class RequestHandler(BaseHTTPRequestHandler):
                 product_name = ilo.get_product_name()
             except:
                 product_name = "Unknown HP Server"
-    		
+
             try:
                 server_name = ilo.get_server_name()
                 if server_name == "":
                     server_name = ilo_host
             except:
-                server_name = ""
-	    
+                server_name = ilo_host
+
             # get health
-	    embedded_health = ilo.get_embedded_health()
+            embedded_health = ilo.get_embedded_health()
             health_at_glance = embedded_health['health_at_a_glance']
             
             if health_at_glance is not None:
